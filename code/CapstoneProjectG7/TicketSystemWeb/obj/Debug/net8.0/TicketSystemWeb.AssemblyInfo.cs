dfc434cd19cd2197032a10993237b20bd4a7f559
Jacob Scott Haas <148472456+Jh00291@users.noreply.github.com>
2025-03-03 18:51:33 -0500

    Fixed Login Bug
    1) There is now user validation for bad credentials when trying to login.
    2) I am now working on trying to fix adding a group to work correctly for me before proceeding to the next bug.

--- code/CapstoneProjectG7/TicketSystemWeb/obj/Debug/net8.0/TicketSystemWeb.AssemblyInfo.cs
+++ code/CapstoneProjectG7/TicketSystemWeb/obj/Debug/net8.0/TicketSystemWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketSystemWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a60e7144ccfe8fd432ef851d8fe7a76020c151")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ce3daa1cfb24a9f9a4b3f1edf1a9d789e2af796")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketSystemWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketSystemWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
