8a5b5115a703f9b5706f1c1230651ec211ea6b57
Dneial <111139207+DRiveraa@users.noreply.github.com>
2025-03-03 23:41:00 -0500

    Kanban Board
    1. created kanban board
    2. can move tickets across columns
    3. can move columns in board
    4. added css to style tickets,columns,kanban

---
+++ code/CapstoneProjectG7/TicketSystemWeb/Models/KanbanBoard/KanbanColumn.cs
@@ -0,0 +1,13 @@
+﻿namespace TicketSystemWeb.Models.KanbanBoard
+{
+    public class KanbanColumn
+    {
+        public int Id { get; set; }
+        public KanbanBoard KanbanBoard { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+        public List<Ticket> Tickets { get; set; } = new();
+        public int KanbanBoardId { get; set; }
+    }
+
+}
