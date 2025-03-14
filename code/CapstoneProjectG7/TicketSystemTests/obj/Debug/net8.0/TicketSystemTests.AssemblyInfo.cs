1de2adeae96a12ca18d38c9b6c905b84967668da
Jacob Scott Haas <148472456+Jh00291@users.noreply.github.com>
2025-03-13 23:28:30 -0400

    Updated Coverage Report
    1) Updated coverage report for Sprint 2

--- code/CapstoneProjectG7/TicketSystemTests/obj/Debug/net8.0/TicketSystemTests.AssemblyInfo.cs
+++ code/CapstoneProjectG7/TicketSystemTests/obj/Debug/net8.0/TicketSystemTests.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketSystemTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b660df885dbb9bef704261b9d4d1144764130e8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eabf5a2a802efe1ccba515ccfd2ce1fde6feee2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketSystemTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketSystemTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
